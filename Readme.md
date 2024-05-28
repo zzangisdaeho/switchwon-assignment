d8ea6fe5bdebba83280ab6bd03fd9e666e6cc425
daeho <zzangisdaeho@naver.com>
2024-05-28 18:34:07 +0900

    context path, api 경로 추가

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
 # PaymentImplApplication 실행
 - build시 실행되도록 하였으나 혹시나 Dto 관련 compile에러가 발생할 시 generateAvroJava로 source generate해주세요
+
+- profile h2 로 실행필요
--- payment-impl/src/main/java/com/switchwon/paymentimpl/payment/in/PaymentController.java
+++ payment-impl/src/main/java/com/switchwon/paymentimpl/payment/in/PaymentController.java
@@ -1,16 +1,38 @@
 package com.switchwon.paymentimpl.payment.in;
 
-import com.switchwon.avro.TestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("payment")
 public class PaymentController {
 
     @GetMapping("/test")
     public String test(){
+        return "test";
+    }
+
+    @GetMapping("/test2")
+    public Object test2(){
+        return "test";
+    }
+
+    @GetMapping("/balance")
+    public String getBalance(){
+        return "test";
+    }
+
+    @GetMapping("/estimate")
+    public String estimate(){
+        return "test";
+    }
+
+    @PostMapping("/approval")
+    public String approval(){
         return "test";
     }
 
